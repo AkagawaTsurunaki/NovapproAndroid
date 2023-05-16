bde6913c908e3cba3fea5a2eb3e6bada124b6dea
AkagawaTsurunaki <965145619@qq.com>
2023-05-16 11:21:37 +0800

    feat: ResponseUtil实现了getServiceMessage方法

--- app/src/main/java/com/github/akagawatsurunaki/android/novapproandroid/util/ResponseUtil.kt
+++ app/src/main/java/com/github/akagawatsurunaki/android/novapproandroid/util/ResponseUtil.kt
@@ -4,53 +4,85 @@ import android.util.Log
 import com.alibaba.fastjson2.JSONObject
 import com.alibaba.fastjson2.TypeReference
 import com.github.akagawatsurunaki.android.novapproandroid.model.ServiceMessage
+import okhttp3.Response
+import java.io.File
 
 object ResponseUtil {
+
+    private val defaultServiceMessage = ServiceMessage(ServiceMessage.Level.FATAL, "默认服务响应，未知错误导致的服务失败")
+    private val defaultResult = Pair(
+        defaultServiceMessage,
+        null
+    )
+
+    fun getServiceMessage(
+        servletValue: String,
+        params: Map<String, String>,
+        fileParams: Map<String, File>
+    ): ServiceMessage {
+        var result: ServiceMessage = defaultServiceMessage
+        Thread {
+            val response = ConnUtil.sendPostRequest(servletValue, params, fileParams)
+            result = parseServiceMessage(response)
+        }.start()
+        return result
+    }
+
+    fun <Model> getServiceResult(
+        servletValue: String,
+        params: Map<String, String>,
+        fileParams: Map<String, File>
+    ): Pair<ServiceMessage, Model?> {
+        var result: Pair<ServiceMessage, Model?> = defaultResult
+        Thread {
+            val response = ConnUtil.sendPostRequest(servletValue, params, fileParams)
+            result = parseResponse<Model>(response)
+        }.start()
+        return result
+    }
 
     fun <Model> getServiceResult(
         servletValue: String,
         params: Map<String, String>
     ): Pair<ServiceMessage, Model?> {
-        var result: Pair<ServiceMessage, Model?>? = null
+        var result: Pair<ServiceMessage, Model?> = defaultResult
         Thread {
             val response = ConnUtil.sendPostRequest(servletValue, params)
-
-            val jsonString = response?.body?.string()
-
-            if (jsonString != null) {
-                val pairType = object : TypeReference<Pair<ServiceMessage, Model>>() {}
-                result = JSONObject.parseObject(jsonString, pairType)
-            } else {
-                Log.e("响应错误", "响应体为NULL")
-            }
-
+            result = parseResponse<Model>(response)
         }.start()
-        return result ?: Pair(
-            ServiceMessage(ServiceMessage.Level.FATAL, "未知错误导致的服务失败"),
-            null
-        )
+        return result
     }
+
     fun <Model> getServiceResult(servletValue: String): Pair<ServiceMessage, Model?> {
-        var result: Pair<ServiceMessage, Model?>? = null
+        var result: Pair<ServiceMessage, Model?> = defaultResult
         Thread {
             val response = ConnUtil.sendGetRequest(servletValue)
-
-            val jsonString = response?.body?.string()
-
-            if (jsonString != null) {
-                val pairType = object : TypeReference<Pair<ServiceMessage, Model>>() {}
-                result = JSONObject.parseObject(jsonString, pairType)
-            } else {
-                Log.e("响应错误", "响应体为NULL")
-            }
-
+            result = parseResponse<Model>(response)
         }.start()
-        return result ?: Pair(
-            ServiceMessage(
-                ServiceMessage.Level.FATAL,
-                "响应体为NULL。请检查URL是否正确，或者服务器是否已经开启，或者类型不匹配等。"
-            ),
-            null
-        )
+        return result
+    }
+
+    private fun parseServiceMessage(response: Response?): ServiceMessage {
+        val jsonString = response?.body?.string()
+        var result: ServiceMessage = defaultServiceMessage
+        if (jsonString != null) {
+            val pairType = object : TypeReference<ServiceMessage>() {}
+            result = JSONObject.parseObject(jsonString, pairType)
+        } else {
+            Log.e("响应错误", "响应体为NULL")
+        }
+        return result
+    }
+
+    private fun <Model> parseResponse(response: Response?): Pair<ServiceMessage, Model?> {
+        val jsonString = response?.body?.string()
+        var result: Pair<ServiceMessage, Model?> = defaultResult
+        if (jsonString != null) {
+            val pairType = object : TypeReference<Pair<ServiceMessage, Model>>() {}
+            result = JSONObject.parseObject(jsonString, pairType)
+        } else {
+            Log.e("响应错误", "响应体为NULL")
+        }
+        return result
     }
 }
