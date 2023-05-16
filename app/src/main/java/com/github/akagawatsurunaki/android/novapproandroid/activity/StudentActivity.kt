1795f2b82c09f3adf35dac7083369ed745c233cd
AkagawaTsurunaki <965145619@qq.com>
2023-05-16 09:08:32 +0800

    feat: 完成了课程申请界面和查看课程申请界面的跳转功能

--- app/src/main/java/com/github/akagawatsurunaki/android/novapproandroid/activity/StudentActivity.kt
+++ app/src/main/java/com/github/akagawatsurunaki/android/novapproandroid/activity/StudentActivity.kt
@@ -1,43 +1,45 @@
 package com.github.akagawatsurunaki.android.novapproandroid.activity
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.github.akagawatsurunaki.android.novapproandroid.activity.ui.theme.NovapproAndroidTheme
+import com.github.akagawatsurunaki.android.novapproandroid.R
+import com.github.akagawatsurunaki.android.novapproandroid.activity.stu.ApplyCourseActivity
+import com.github.akagawatsurunaki.android.novapproandroid.activity.stu.CheckCourseApplicationActivity
+import com.github.akagawatsurunaki.android.novapproandroid.databinding.StudentLayoutBinding
 
 class StudentActivity : ComponentActivity() {
+
+    private val functionalityList = listOf("我要申请课程", "查看我的申请")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            NovapproAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting2("Android")
-                }
+        setContentView(R.layout.student_layout)
+        val binding = StudentLayoutBinding.inflate(layoutInflater)
+
+        val adapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, functionalityList)
+        binding.studentLayoutListViewFunctionalityList.adapter = adapter
+        binding.studentLayoutListViewFunctionalityList.setOnItemClickListener { _, _, position, _ ->
+            when (functionalityList[position]) {
+                "我要申请课程" -> toApplyCourseActivity()
+                "查看我的申请" -> toCheckCourseApplicationActivity()
+                else -> Log.e(StudentActivity::class.java.name, "onCreate: 这是一个不可能被点击到的位置", )
             }
         }
+
     }
-}
 
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun toApplyCourseActivity() {
+        val intent = Intent(this, ApplyCourseActivity::class.java)
+        startActivity(intent)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    NovapproAndroidTheme {
-        Greeting2("Android")
+    private fun toCheckCourseApplicationActivity() {
+        val intent = Intent(this, CheckCourseApplicationActivity::class.java)
+        startActivity(intent)
     }
 }
