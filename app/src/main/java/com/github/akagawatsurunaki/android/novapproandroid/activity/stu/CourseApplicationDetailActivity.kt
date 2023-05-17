5c7ee25aa371356099e3fa13c532d0b3dd6fe3ab
AkagawaTsurunaki <965145619@qq.com>
2023-05-17 20:11:58 +0800

    feat: 学生查看申请详情界面的显示及其功能的实现

---
+++ app/src/main/java/com/github/akagawatsurunaki/android/novapproandroid/activity/stu/CourseApplicationDetailActivity.kt
@@ -0,0 +1,121 @@
+package com.github.akagawatsurunaki.android.novapproandroid.activity.stu
+
+import android.os.Bundle
+import android.widget.TableRow
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import com.github.akagawatsurunaki.android.novapproandroid.databinding.CourseApplicationDetailLayoutBinding
+import com.github.akagawatsurunaki.android.novapproandroid.model.ServiceMessage
+import com.github.akagawatsurunaki.android.novapproandroid.service.appro.ApprovalService
+import com.github.akagawatsurunaki.android.novapproandroid.service.stu.CourseApplicationDetailService
+
+class CourseApplicationDetailActivity : ComponentActivity() {
+
+    private lateinit var binding: CourseApplicationDetailLayoutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = CourseApplicationDetailLayoutBinding.inflate(layoutInflater)
+
+        val flowNo = intent.getStringExtra("flowNo")
+
+        flowNo?.let {
+            // 初始化界面
+            initAppliedCourses(it)
+            initApprovalFlowDetails(it)
+
+            // 绑定按钮事件
+            binding.buttonApprovalConfirm.setOnClickListener { _ ->
+                ApprovalService.tryFinishApprovalFlow(it, "确定")
+            }
+        }
+    }
+
+    private fun initApprovalFlowDetails(flowNo: String) {
+        // 检查服务响应
+        val getApprovalFlowDetailsServiceMessage = ApprovalService.getApprovalFlowDetails(flowNo)
+        if (getApprovalFlowDetailsServiceMessage.first.level != ServiceMessage.Level.SUCCESS) {
+            Toast.makeText(
+                this,
+                getApprovalFlowDetailsServiceMessage.first.message,
+                Toast.LENGTH_LONG
+            )
+                .show()
+            return
+        }
+
+        // 动态加载申请流明细
+        val approvalFlowDetails = getApprovalFlowDetailsServiceMessage.second ?: emptyList()
+        approvalFlowDetails.forEach {
+            binding.tableLayoutApprovalFlowDetails.apply {
+                addView(
+                    TableRow(this@CourseApplicationDetailActivity).apply {
+                        addView(TextView(this@CourseApplicationDetailActivity).apply {
+                            text = it.flowNo
+                        })
+                        addView(TextView(this@CourseApplicationDetailActivity).apply {
+                            text = it.title
+                        })
+                        addView(TextView(this@CourseApplicationDetailActivity).apply {
+                            text = it.applicantId.toString()
+                        })
+                        addView(TextView(this@CourseApplicationDetailActivity).apply {
+                            text = it.applicantName
+                        })
+                        addView(TextView(this@CourseApplicationDetailActivity).apply {
+                            text = it.addTime.toString()
+                        })
+                        addView(TextView(this@CourseApplicationDetailActivity).apply {
+                            text = it.approStatus!!.chinese
+                        })
+                        addView(TextView(this@CourseApplicationDetailActivity).apply {
+                            text = it.applCourses.toString()
+                        })
+                    }
+                )
+            }
+        }
+    }
+
+    private fun initAppliedCourses(flowNo: String) {
+        // 检查服务响应
+        val getAppliedCoursesServiceMessage =
+            CourseApplicationDetailService.getAppliedCourses(flowNo)
+        if (getAppliedCoursesServiceMessage.first.level != ServiceMessage.Level.SUCCESS) {
+            Toast.makeText(this, getAppliedCoursesServiceMessage.first.message, Toast.LENGTH_LONG)
+                .show()
+            return
+        }
+
+        // 动态加载已申请的课表
+        val appliedCourses = getAppliedCoursesServiceMessage.second ?: emptyList()
+        appliedCourses.forEach {
+            binding.tableLayoutAppliedCourses.apply {
+                addView(
+                    TableRow(this@CourseApplicationDetailActivity).apply {
+                        addView(TextView(this@CourseApplicationDetailActivity).apply {
+                            text = it.code
+                        })
+                        addView(TextView(this@CourseApplicationDetailActivity).apply {
+                            text = it.name
+                        })
+                        addView(TextView(this@CourseApplicationDetailActivity).apply {
+                            text = it.serialNumber
+                        })
+                        addView(TextView(this@CourseApplicationDetailActivity).apply {
+                            text = it.teachers
+                        })
+                        addView(TextView(this@CourseApplicationDetailActivity).apply {
+                            text = it.onlineContactWay
+                        })
+                        addView(TextView(this@CourseApplicationDetailActivity).apply {
+                            text = it.comment
+                        })
+                    }
+                )
+            }
+        }
+    }
+}
