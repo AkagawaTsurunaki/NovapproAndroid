8560ffa4dd85e1c1ea5e089609f5f77f95042503
AkagawaTsurunaki <965145619@qq.com>
2023-05-18 10:19:12 +0800

    feat: 管理员界面的雏形和功能框架

--- app/src/main/java/com/github/akagawatsurunaki/android/novapproandroid/activity/admin/AdminActivity.kt
+++ app/src/main/java/com/github/akagawatsurunaki/android/novapproandroid/activity/admin/AdminActivity.kt
@@ -1,46 +1,61 @@
 package com.github.akagawatsurunaki.android.novapproandroid.activity.admin
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.github.akagawatsurunaki.android.novapproandroid.activity.ui.theme.NovapproAndroidTheme
+import com.github.akagawatsurunaki.android.novapproandroid.R
+import com.github.akagawatsurunaki.android.novapproandroid.activity.stu.StudentActivity
+import com.github.akagawatsurunaki.android.novapproandroid.databinding.AdminLayoutBinding
+import com.github.akagawatsurunaki.android.novapproandroid.databinding.StudentLayoutBinding
 
 class AdminActivity : ComponentActivity() {
+
+    private val functionalityList =
+        listOf(
+            "系统用户管理",
+            "课申请课程管理",
+            "用户审批权限管理",
+            "审批记录查询"
+        )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            NovapproAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting3("Android")
-                }
+        setContentView(R.layout.admin_layout)
+        val binding = AdminLayoutBinding.inflate(layoutInflater)
+
+        val adapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, functionalityList)
+        binding.listViewAdminFunctionalities.adapter = adapter
+        binding.listViewAdminFunctionalities.setOnItemClickListener { _, _, position, _ ->
+            when (functionalityList[position]) {
+                "系统用户管理" -> toSystemUserManagementActivity()
+                "课申请课程管理" -> toCourseManagementActivity()
+                "用户审批权限管理" -> toUserManagementActivity()
+                "审批记录查询" -> toApprovalFlowQueryActivity()
+                else -> Log.e(
+                    StudentActivity::class.java.name,
+                    "onCreate: 这是一个不可能被点击到的位置",
+                )
             }
         }
     }
-}
 
-@Composable
-fun Greeting3(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview3() {
-    NovapproAndroidTheme {
-        Greeting3("Android")
+    private fun toSystemUserManagementActivity() {
+        startActivity(Intent(this, SystemUserManagementActivity::class.java))
     }
+
+    private fun toCourseManagementActivity() {
+        startActivity(Intent(this, CourseManagementActivity::class.java))
+    }
+
+    private fun toUserManagementActivity() {
+        startActivity(Intent(this, UserManagementActivity::class.java))
+    }
+
+    private fun toApprovalFlowQueryActivity() {
+    startActivity(Intent(this, ApprovalFlowQueryActivity::class.java))
+    }
+
 }
