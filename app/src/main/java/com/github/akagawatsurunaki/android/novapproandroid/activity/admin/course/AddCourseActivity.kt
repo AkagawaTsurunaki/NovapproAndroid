68f2375cdd2948db16a44ed1bda07294a7d0f512
AkagawaTsurunaki <965145619@qq.com>
2023-05-18 23:26:01 +0800

    feat: 新建课程和修改课程的页面布局文件

---
+++ app/src/main/java/com/github/akagawatsurunaki/android/novapproandroid/activity/admin/course/AddCourseActivity.kt
@@ -0,0 +1,22 @@
+package com.github.akagawatsurunaki.android.novapproandroid.activity.admin.course
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.akagawatsurunaki.android.novapproandroid.activity.admin.course.ui.theme.NovapproAndroidTheme
+
+class AddCourseActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+}
