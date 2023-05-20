21ea3129109665530680c017e6d1004b769dbd74
AkagawaTsurunaki <965145619@qq.com>
2023-05-20 17:16:58 +0800

    fix: 修复了数组越界的bug

--- app/src/main/java/com/github/akagawatsurunaki/android/novapproandroid/util/SessionUtil.kt
+++ app/src/main/java/com/github/akagawatsurunaki/android/novapproandroid/util/SessionUtil.kt
@@ -3,9 +3,13 @@ package com.github.akagawatsurunaki.android.novapproandroid.util
 import okhttp3.Response
 
 object SessionUtil {
-    fun getSession(res: Response?) = res?.run {
-        val session = res.headers.values("Set-Cookie")[0]
-        session.substring(0, session.indexOf(";"))
+    fun getSession(res: Response?): String? {
+        val cookies = res?.headers?.values("Set-Cookie")
+        if (!cookies.isNullOrEmpty()) {
+            val session = cookies[0]
+            return session.substring(0, session.indexOf(";"))
+        }
+        return null
     }
 
 }
