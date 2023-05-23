86f194a34ac3324f9e59aa70c9c8bd4095eef85d
AkagawaTsurunaki <965145619@qq.com>
2023-05-23 08:53:59 +0800

    feat: 使用SQLite存储用户数据

---
+++ app/src/main/java/com/github/akagawatsurunaki/android/novapproandroid/helper/MyDbHelper.kt
@@ -0,0 +1,24 @@
+package com.github.akagawatsurunaki.android.novapproandroid.helper
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+class MyDbHelper (private val context: Context, name: String, version: Int) :
+
+    SQLiteOpenHelper(context, name, null, version) {
+
+    private val TAG = "MyDbHelper"
+    private val createUserInfo = "create table UserInfo (" +
+            " id userId primary key autoincrement," +
+            "username text," +
+            "usertype text," +
+            "theme text)"
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(createUserInfo)
+        Log.i(TAG, "数据库初始化完成")
+    }
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+    }
+}
